b6e6d93e23992b05cbb140999f53aa199621809c
Datka Mamatzhanova <datkascnu@gmail.com>
2023-04-05 11:46:58 +0600

    Added constructor and getter setter in School class and added Teacher class

--- src/model/School.java
+++ src/model/School.java
@@ -3,4 +3,25 @@ package model;
 public class School {
     private Long id;
     private String name;
+
+    public School(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
---
+++ src/model/Teacher.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Teacher {
+
+    private Long id;
+    private String fullName;
+    private int age;
+}
